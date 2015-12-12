8a5e295187340e4f1d6b36df752e5334cf5a9e66
Matthew Leibowitz <mattleibow@live.com>
2015-12-12 02:56:24 +0200

    Updated SimpleJitterPhoneDemo to use MonoGame and the new portable Jitter

--- samples/JitterPhoneDemo/SimpleJitterPhoneDemo/Properties/AssemblyInfo.cs
+++ samples/JitterPhoneDemo/SimpleJitterPhoneDemo/Properties/AssemblyInfo.cs
@@ -1,28 +1,27 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SimpleJitterPhoneDemo")]
-[assembly: AssemblyProduct("SimpleJitterPhoneDemo")]
 [assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyCopyright("Copyright ©  2010")]
+[assembly: AssemblyProduct("SimpleJitterPhoneDemo")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type. Only Windows
-// assemblies support COM.
+// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// On Windows, the following GUID is for the ID of the typelib if this
-// project is exposed to COM. On other platforms, it unique identifies the
-// title storage container when deploying this assembly to the device.
-[assembly: Guid("decb928d-d049-41ba-9476-39c6f4d869ac")]
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e7781151-8802-4883-bab0-9b823f61a8e1")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,4 +30,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
