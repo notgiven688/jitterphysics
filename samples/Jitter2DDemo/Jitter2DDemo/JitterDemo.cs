7f542733c66f5fe3826670d186b13ab1d3907725
Matthew Leibowitz <mattleibow@live.com>
2015-12-12 18:52:25 +0200

    Updated Jitter2D Demo to use MonoGame and the new portable Jitter 2D

--- samples/Jitter2DDemo/Jitter2DDemo/JitterDemo.cs
+++ samples/Jitter2DDemo/Jitter2DDemo/JitterDemo.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
@@ -106,7 +105,7 @@ namespace JitterDemo
 
             foreach (Type type in Assembly.GetExecutingAssembly().GetTypes())
             {
-                if (type.Namespace == "JitterDemo.Scenes" && !type.IsAbstract)
+                if (type.Namespace == "JitterDemo.Scenes" && !type.IsAbstract && type.DeclaringType == null)
                 {
                     if (type.Name == "Pyramid") currentScene = PhysicScenes.Count;
                     Scenes.Scene scene = (Scenes.Scene)Activator.CreateInstance(type, this);
