55d5a853e7ec418542e90a8c128f20af70b6b7c4
Matthew Leibowitz <mattleibow@live.com>
2015-12-12 20:55:03 +0200

    Getting the build bits ready for v0.2

---
+++ build.bat
@@ -0,0 +1,23 @@
+@echo off
+
+rem clean up any files / folders
+if not exist output mkdir output
+del /S /Q output
+for /D %%p in ("output\*") do rmdir "%%p" /s /q
+
+
+rem build 3D library
+msbuild source\Jitter.sln /p:Configuration=Release /t:Rebuild
+
+rem ready for packaging
+if not exist output\net4 mkdir output\net4
+if not exist output\Portable mkdir output\Portable
+copy source\Jitter\bin\net4\Release\Jitter.dll output\net4\
+copy source\Jitter\bin\net4\Release\Jitter.pdb output\net4\
+copy source\Jitter\bin\net4\Release\Jitter.xml output\net4\
+copy source\Jitter\bin\Portable\Release\Jitter.dll output\Portable\
+copy source\Jitter\bin\Portable\Release\Jitter.pdb output\Portable\
+copy source\Jitter\bin\Portable\Release\Jitter.xml output\Portable\
+
+rem package
+nuget pack nuget\Jitter.nuspec -OutputDirectory output
--- source/Jitter/Properties/AssemblyInfo.cs
+++ source/Jitter/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Jitter")]
+[assembly: AssemblyTitle("Jitter Physics 3D")]
 [assembly: AssemblyProduct("Jitter")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Realtime physics engine for .NET")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyCopyright("Copyright © 2011 Xamarin Inc.")]
+[assembly: AssemblyCopyright("Copyright (c) 2015 Thorben Linneweber, Matthew Leibowitz")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,3 +21,4 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 [assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
